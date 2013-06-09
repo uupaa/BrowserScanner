2378e6a64f90b04961ae7d7564ed1bf35a129eb3
uupaa <uupaa.js@gmail.com>
2013-06-10 06:42:44 +0900

    update

--- case/Canvas.js
+++ case/Canvas.js
@@ -1,15 +1,6 @@
 result.push({
-    Class: "URL", id: "window.URL",
-    state: !!(window.URL || window.webkitURL),
+    Class: "Canvas", id: "document.getCSSCanvasContext",
+    state: !!document.getCSSCanvasContext,
     spec: "https://gist.github.com/uupaa/4445734"
 });
-result.push((function() {
-    var url = window.URL || window.webkitURL;
-
-    return {
-        Class: "URL", id: "window.URL.createObjectURL",
-        state: !!(url.createObjectURL),
-        spec: "https://gist.github.com/uupaa/4445734"
-    };
-})());
 
--- case/PageVisibility.js
+++ case/PageVisibility.js
@@ -1,11 +1,11 @@
 result.push({ Class: "PageVisibility", id: "PageVisibility",  state: PageVisibility(), spec: "http://www.w3.org/TR/page-visibility/" });
 
 function PageVisibility() {
-    if ("webkitHidden" in document) {
-        return true;
-    }
     if ("hidden" in document) {
         return true;
+    }
+    if ("webkitHidden" in document) {
+        return "webkitHidden";
     }
     return false;
 }
